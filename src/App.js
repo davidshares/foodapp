da96eb23045dc081785b12b262826471bb73ada4
David Shares <davidshares@outlook.com>
2019-11-25 21:41:49 -0500

    updating app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import dog from './550_chaser.jpg';
 
 {/* Template
 function Template(props) {
@@ -54,8 +55,10 @@ function App() {
                 answer="No, I don't."
             />
 
+            <img src={dog} className="dog" alt="border-collie" />
+
             <Story
-                story="Border collies are known for their intelligence. Chaser is a border collie that belongs to a retired psychology professor. John W. Pilley read about a dog who learned to recognize 200 German nouns, and decided to see if Chaser could learn words as well. Working with the dog for around five hours a day, Chaser learned to recognize the names of a couple of new objects every day, to a current total of 1,022 nouns! Along the way, Chaser got the idea that learning words is her job, so now the 82-year-old Pilley has a hard time taking it easy, as Chaser demands her lessons! Pilley is now teaching Chaser verbs and basic grammar."
+                story="Maybe you should learn more about dogs? Border collies are known for their intelligence. Chaser is a border collie that belongs to a retired psychology professor. John W. Pilley read about a dog who learned to recognize 200 German nouns, and decided to see if Chaser could learn words as well. Working with the dog for around five hours a day, Chaser learned to recognize the names of a couple of new objects every day, to a current total of 1,022 nouns! Along the way, Chaser got the idea that learning words is her job, so now the 82-year-old Pilley has a hard time taking it easy, as Chaser demands her lessons! Pilley is now teaching Chaser verbs and basic grammar."
             />
 
         </div>
